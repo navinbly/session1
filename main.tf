a5b80bcfd52ed76915523c5d8ff6bcbbe451a90e
navinbly <151385759+navinbly@users.noreply.github.com>
2024-04-13 15:25:47 +0200

    Add files via upload
    First Version - 13-04-2024

---
+++ main.tf
@@ -0,0 +1,89 @@
+
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.10.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "navin-terraform-training"
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
+}
+
+resource "google_compute_instance" "vm_instance1" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "vm_instance2" {
+  name         = "terraform-instance2"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+resource "google_compute_instance" "instance-20240410-164112" {
+  # (resource arguments)
+  
+    # A default network is created for all GCP projects
+    name         = "instance-20240410-164112"
+machine_type = "e2-micro"
+
+boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+#resource "google_compute_instance" "vvm_instance_new" {
+#  name         = "terraform-instance-new"
+#  machine_type = "e2-micro"
+
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-11"
+#    }
+#  }
+
+#  network_interface {
+#    # A default network is created for all GCP projects
+#    network = "default"
+#    access_config {
+#    }
+#  }
+#}
+
